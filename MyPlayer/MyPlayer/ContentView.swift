0f14d238ad2224ddc79cf48480271a309f5783c5
chengqian <chartype@163.com>
2021-09-02 01:27:01 +0800

    初始化项目

---
+++ MyPlayer/MyPlayer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyPlayer
+//
+//  Created by 程倩的MacBook on 2021/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
